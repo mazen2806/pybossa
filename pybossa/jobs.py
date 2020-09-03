9ef5e8ba549b49ecb3e2e4e491cc17d1ceeec9fa
Yurii Biurher <yurii.biurher@zorallabs.com>
2020-09-03 09:31:46 +0300

    added more info to webhook failure email

--- pybossa/jobs.py
+++ pybossa/jobs.py
@@ -577,9 +577,8 @@ def webhook(url, payload=None, oid=None, rerun=False):
     finally:
         if project.published and webhook.response_status_code != 200 and current_app.config.get('ADMINS'):
             subject = "Broken: %s webhook failed" % project.name
-            body = 'Sorry, but the webhook failed'
-            mail_dict = dict(recipients=current_app.config.get('ADMINS'),
-                             subject=subject, body=body, html=webhook.response)
+            body = 'Sorry, but the webhook failed.\n\n' + 'webhook url = ' + str(url) + '\nresponse_status_code = ' + str(webhook.response_status_code) + '\nresponse:\n\n' + str(webhook.response) + '\n\npayload:\n\n' + str(payload)
+            mail_dict = dict(recipients=current_app.config.get('ADMINS'), subject=subject, body=body)
             send_mail(mail_dict)
     if current_app.config.get('SSE'):
         publish_channel(sentinel, payload['project_short_name'],
